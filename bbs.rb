6422af95ef8068374f50a1f829ce3db8c4634ac2
Philipp Neugebauer <ppn@philippneugebauer.net>
2018-10-07 13:39:00 +0200

    bubble sort added

---
+++ bbs.rb
@@ -0,0 +1,25 @@
+class Bbs
+
+  def self.s
+    b = Bbs.new
+    array = [5,8,1,2,4,3]
+    puts "array: [#{array.join(", ")}]"
+    result = b.bubble_sort array
+    puts "array: [#{result.join(", ")}]"
+  end
+
+  def bubble_sort(array)
+    [*1..array.length - 1].reverse_each do |i|
+      swapped = false
+      [*0..i - 1].each do |j|
+        if array[j] > array[j + 1]
+          array[j + 1], array[j] = array[j], array[j + 1]
+          swapped = true
+        end
+      end
+      break if swapped == false
+    end
+    array
+  end
+
+end
