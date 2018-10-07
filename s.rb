622dba4f890aa5315b3f7e89fcd7e512aa46095c
Philipp Neugebauer <ppn@philippneugebauer.net>
2018-10-07 13:39:09 +0200

    selection sort added

---
+++ s.rb
@@ -0,0 +1,34 @@
+class S
+
+  def self.s
+    b = S.new
+    array = [5,8,1,2,4,3]
+    puts "array: [#{array.join(", ")}]"
+    result = b.selection_sort array
+    puts "array: [#{result.join(", ")}]"
+  end
+
+  def selection_sort(array)
+    array_length = array.length - 1
+    [*0..array_length].each do |i|
+      min_index = i
+      min = array[i]
+      [*i+1..array_length].each do |j|
+        min_index, min = j, array[j] if min > array[j]
+      end
+      array[min_index], array[i] = array[i], array[min_index]
+    end
+    array
+  end
+
+  def ruby_selection_sort(array)
+    array_length = array.length - 1
+    [*0..array_length].each do |i|
+      a = array.slice i, array_length
+      min_index = a.index a.min
+      array[i + min_index], array[i] = array[i], array[i + min_index]
+    end
+    array
+  end
+
+end
